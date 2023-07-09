7df7a1810146d4b889f966188dda5f5cd5f8889d
unknown <mariamatef226@gmail.com>
2023-07-09 04:33:31 +0300

    finished part 1 of the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,46 @@
-import './App.css'
-import Die from './components/Die'
-import {useState, useEffect} from 'react'
+import "./App.css";
+import Die from "./components/Die";
+import { useState, useEffect } from "react";
 
 function App() {
-    let [values,setValues] = useState([]);
+  let [values, setValues] = useState(()=>{
+    let temp = [];
+    for (let i = 0; i < 10; i++)
+      temp.push({value: Math.round(Math.random(0, 1) * 5 + 1), isHeld:false});
+    return temp
+  });
 
-    useEffect(()=>{
-      let temp = [];
-      for (let i=0; i<10;i++)
-        temp.push(Math.round(Math.random(0,1)*5 +1))
-      setValues(temp);
-      console.log(values)
-    },[]);
+function roll(){
+  let temp = [];
+  for (let i = 0; i < 10; i++){
+    if (!values[i].isHeld)
+      temp.push({value:Math.round(Math.random(0, 1) * 5 + 1), isHeld:false});
+    else
+      temp.push(values[i])  
+  setValues(temp);
+}
+}
+
+function toggle(index){
+  setValues((prev)=>prev.map((v,i)=>{ return index == i ? {...v, isHeld:!v.isHeld}: {...v} }));
+    
+}
 
 
   return (
     <>
-     <main className='container'>
-      {values.map(v=>{
-        return <Die value={v} />
-      })}       
- 
-     </main>
+      <main className='text-center'>
+        <div className="container">
+          {values.map((v, index) => {
+            return <Die value={v.value} index={index} key={index} clickHandler={toggle}  held={v.isHeld}/>;
+          })}
+        </div>
+        <button onClick={roll} className="btn-primary btn text-light">Roll</button>
+
+      </main>
     </>
-  )
+  );
+
 }
 
-export default App
+export default App;
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,7 @@
 export default function Die(props){
-
+    let bgcolor = props.held? '#59e390':'white';
 
     return (
-        <div className='die '>{props.value}</div>
+        <div className='die' style={{backgroundColor:bgcolor}} onClick={()=>props.clickHandler(props.index)}>{props.value}</div>
     )
 }
